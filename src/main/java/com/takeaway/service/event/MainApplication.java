b9beb830f45017aa90a7bda8be2d374c74c150c2
Sunday Take Away <eugene.le.roux@icloud.com>
2018-08-02 05:14:24 +0200

    wired-in basic swagger integration

--- src/main/java/com/takeaway/service/event/MainApplication.java
+++ src/main/java/com/takeaway/service/event/MainApplication.java
@@ -3,8 +3,10 @@ package com.takeaway.service.event;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class MainApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
